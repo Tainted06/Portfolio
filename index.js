8158634e61f455b46e286403fbc7991f8d9e6071
Tainted06 <80160851+Tainted06@users.noreply.github.com>
2022-01-25 16:12:06 +0300

    Finished Version 1

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-/*!
-* Start Bootstrap - Resume v7.0.4 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
@@ -17,8 +6,6 @@ window.addEventListener('DOMContentLoaded', event => {
             offset: 74,
         });
     };
-
-    // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
@@ -30,5 +17,4 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-
 });
